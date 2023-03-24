a0ffa87045998209eb831865dc6c17b47f655391
mikhael2003 <mikhaelsiman2003@gmail.com>
2023-03-24 14:25:37 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -29,8 +29,15 @@ select datediff(day, '2023-05-07', '2023-06-08')
 select convert(date, '2023-09-20')
 select convert(datetime, '2023-09-20')
 
+select * from tbmhs
 
+ALTER TABLE tbmhs ADD hr_lahir varchar (50),umur int,thn_kuliah varchar (50),lama_kuliah varchar (50);
+alter table tbmhs add thn_lulus varchar (50);
+ALTER TABLE tbmhs ALTER COLUMN thn_kuliah int ;
+ALTER TABLE tbmhs ALTER COLUMN thn_lulus int ;
 
-
-
-
+update tbmhs set hr_lahir= datename(dw, tgl_lahir); 
+update tbmhs set umur = year(getdate())-year(tgl_lahir);
+update tbmhs set thn_kuliah = left(npm,2)+2000;
+update tbmhs set thn_lulus = (substring(npm,5,2)+2000)
+update tbmhs set lama_kuliah= thn_lulus-thn_kuliah;
